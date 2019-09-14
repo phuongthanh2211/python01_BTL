ce727a4f6f2e9799522be120ce84c1035efd313c
phuongthanh2211 <phuongthanh2211@gmail.com>
2019-09-14 13:36:45 +0000

    init commit

---
+++ BTL/hoadonbanhang.py
@@ -0,0 +1,13 @@
+print("+--------------MENU-----------------+")
+print("|Chon C de tao hoa don              |")
+print("|Chon R de xem thong tin hoa don    |")
+print("|Chon T de tinh tong doanh thu      |")
+print("|Chon A de tinh tong hang hoa ban ra|")
+print("|Chon E de thoat                    |")
+print("+-----------------------------------+")
+
+# danhsachhoadon=[]
+danhsachhoadon = [
+    {'thue': 0.1, 'tongtien': 27500.0, 'danhsachhanghoa': [{'ten': 'Coca', 'dongia': 5000, 'thanhtien': 10000, 'stt': '1', 'soluong': 2}, {'ten': 'Pepsi', 'dongia': 3000, 'thanhtien': 15000, 'stt': '2', 'soluong': 5}], 'nguoimua': 'Cuong', 'ngayhoadon': '1', 'sohoadon': 'MS01', 'tongtientruocthue': 25000},
+    {'thue': 0.1, 'tongtien': 27500.0, 'danhsachhanghoa': [{'ten': 'Coca', 'dongia': 5000, 'thanhtien': 10000, 'stt': '1', 'soluong': 2}, {'ten': 'Pepsi', 'dongia': 3000, 'thanhtien': 15000, 'stt': '2', 'soluong': 5}], 'nguoimua': 'Cuong', 'ngayhoadon': '1', 'sohoadon': 'MS02', 'tongtientruocthue': 25000}
+]
